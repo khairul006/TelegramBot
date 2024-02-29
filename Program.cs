ce7dd072a95f76af53de9343af7f145e50ddc8f3
Khai <khairulzamidi2000@gmail.com>
2024-02-29 23:28:48 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Telegram.Bot;
+
+namespace TelegramBot 
+{
+    class Program
+    {
+        public static TelegramBotClient Client;
+
+        [Obsolete]
+        static void Main(string[] args)
+        {
+            Client = new TelegramBotClient("7009725948:AAFI0Zd7MkMbM_AYzvkvpJmzHrHgHEFCQ6s");
+            Client.OnMessage += Client_OnMessage;
+            Client.StartReceiving();
+            Thread.Sleep(-1);
+        }
+
+        [Obsolete]
+        private static void Client_OnMessage(object? sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+            var id = e.Message.Chat.Id;
+            var text = e.Message?.Text;
+
+            Client.SendTextMessageAsync(id, $"You said: {text}");
+        }
+
+       
+    }
+}
+
